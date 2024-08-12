f13c6892f64939554cf969e67b4b06100e422ad2
guiaf04 <guilhermearaujo@alu.ufc.br>
2024-08-11 22:19:07 -0300

    feat: add CRD operations

---
+++ src/TodoList.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TodoList{
+  private List<Task> taskList = new ArrayList<>();
+
+  public void addTask(String name, String description, int priorityLevel, String category){
+    Task task = new Task(name, description, priorityLevel, category);
+    taskList.add(task);
+    Collections.sort(taskList);
+  }
+
+  public void deleteTask(String name){
+    List<Task> tasksForRemove = new ArrayList<>();
+
+    if(!taskList.isEmpty() && name != null){
+      for(Task t : taskList){
+        if(t.getName().equals(name)){
+          tasksForRemove.add(t);
+        }
+      }
+
+      taskList.removeAll(tasksForRemove);
+    }
+  }
+
+  public void editTask(){
+    //TODO
+  }
+
+  public void showList(){
+    System.out.println();
+    for(Task t : taskList){
+      System.out.println(t);
+    }
+  }
+
+}
